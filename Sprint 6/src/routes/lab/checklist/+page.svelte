4389c1c27096de80bdea465ac27458fcc0834d00
Dylan Crimmings <c00273675@setu.ie>
2025-10-06 12:54:27 +0100

    step 4

--- Sprint 6/src/routes/lab/checklist/+page.svelte
+++ Sprint 6/src/routes/lab/checklist/+page.svelte
@@ -2,32 +2,10 @@
     import ChecklistItem from '$lib/components/ChecklistItem.svelte';
     import ChecklistProgress from "$lib/components/ChecklistProgress.svelte";
 
-    let counterAmount = 0;
-
-    let checkboxItems = 1;
-
-    function updateCounter(e){
-        if (e.detail.done === true){
-            counterAmount++;
-        }
-        else{
-            counterAmount--;
-        }
-    }
-
-
 </script>
 
 <main>
     <h1 data-testid="title">Progress Checklist</h1>
-
-    <h2 data-testid="counter" id="counter">{counterAmount}</h2>
-
-    <div id="checklist">
-        {#each {length: checkboxItems} as _, i}
-            <ChecklistItem id="checklistItem-{i}" label="Item {i}" on:change={updateCounter}></ChecklistItem>
-        {/each}
-    </div>
 
     <ChecklistProgress></ChecklistProgress>
 </main>
--- Sprint 6/tests/tests.spec.ts
+++ Sprint 6/tests/tests.spec.ts
@@ -6,20 +6,6 @@ test('has title', async ({ page }) => {
     await expect(page.getByTestId("title")).toHaveText("Progress Checklist");
 });
 
-test('checkbox item', async ({page}) => {
-    await page.goto('http://localhost:5173/lab/checklist');
-
-    let counterAmount:number = Number(await page.getByTestId("counter").innerText());
-
-    await page.waitForTimeout(500);
-
-    await page.getByTestId("checklistItem-0").check();
-    await expect(page.getByTestId("checklistItem-0")).toBeChecked();
-
-    let expectedAmount:number = counterAmount + 1;
-    await expect(page.getByTestId("counter")).toHaveText(expectedAmount.toString());
-})
-
 test('progress submission', async ({page}) => {
     await page.goto('http://localhost:5173/lab/checklist');
 
@@ -42,4 +28,91 @@ test('progress submission', async ({page}) => {
     await page.waitForTimeout(500);
 
     await expect(page.getByTestId("progress")).toHaveText("2/5 (40%)");
+})
+
+test('progress submission change', async ({page}) => {
+    await page.goto('http://localhost:5173/lab/checklist');
+
+    await expect(page.getByTestId("progress")).toHaveText("0/5 (0%)");
+
+    await page.waitForTimeout(500);
+
+    await page.getByTestId("checklistItem-1").check();
+    await expect(page.getByTestId("checklistItem-1")).toBeChecked();
+
+    await page.getByTestId("checklistItem-4").check();
+    await expect(page.getByTestId("checklistItem-4")).toBeChecked();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("0/5 (0%)");
+
+    await page.getByTestId("submit").click();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("2/5 (40%)");
+
+    await page.waitForTimeout(500);
+
+    await page.getByTestId("checklistItem-1").uncheck();
+    await expect(page.getByTestId("checklistItem-1")).not.toBeChecked();
+
+    await page.getByTestId("checklistItem-2").check();
+    await expect(page.getByTestId("checklistItem-2")).toBeChecked();
+
+    await page.getByTestId("checklistItem-3").check();
+    await expect(page.getByTestId("checklistItem-3")).toBeChecked();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("2/5 (40%)");
+
+    await page.getByTestId("submit").click();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("3/5 (60%)");
+
+    await page.getByTestId("checklistItem-0").check();
+    await expect(page.getByTestId("checklistItem-0")).toBeChecked();
+
+    await page.getByTestId("checklistItem-1").check();
+    await expect(page.getByTestId("checklistItem-1")).toBeChecked();
+
+    await expect(page.getByTestId("progress")).toHaveText("3/5 (60%)");
+
+    await page.getByTestId("submit").click();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("5/5 (100%)");
+
+    await page.waitForTimeout(500);
+
+    await page.getByTestId("checklistItem-0").uncheck();
+    await expect(page.getByTestId("checklistItem-0")).not.toBeChecked();
+
+    await page.getByTestId("checklistItem-1").uncheck();
+    await expect(page.getByTestId("checklistItem-1")).not.toBeChecked();
+
+    await page.getByTestId("checklistItem-2").uncheck();
+    await expect(page.getByTestId("checklistItem-2")).not.toBeChecked();
+
+    await page.getByTestId("checklistItem-3").uncheck();
+    await expect(page.getByTestId("checklistItem-3")).not.toBeChecked();
+
+    await page.getByTestId("checklistItem-4").uncheck();
+    await expect(page.getByTestId("checklistItem-4")).not.toBeChecked();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("5/5 (100%)");
+
+    await page.getByTestId("submit").click();
+
+    await page.waitForTimeout(500);
+
+    await expect(page.getByTestId("progress")).toHaveText("0/5 (0%)");
+
 })
